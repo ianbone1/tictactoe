019f80f5c044f8e81a1d189474756f2738030339
Ian Bone <ianbone1@googlemail.com>
2019-03-15 13:08:41 +0000

    Working solution

--- player/views.py
+++ player/views.py
@@ -12,8 +12,12 @@ from django.core.exceptions import PermissionDenied
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
+    finished_games = my_games.difference(active_games)
     invitations = request.user.invitations_received.all()
-    return render(request, "player/home.html", {'games': active_games, 'invitations': invitations})
+    return render(request, "player/home.html",
+                  {'active_games': active_games,
+                   'finished_games': finished_games,
+                   'invitations': invitations})
 
 @login_required
 def new_invitation(request):
